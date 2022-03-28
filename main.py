eb18a1adcc9674557f093ef2ff1483fde2e6f6ee
Kitsu <krasilnikovkolya4654@gmail.com>
2022-03-28 22:11:23 +0300

    Made resolution array

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 
+# Resolution
+WIDTH = 170
+HEIGHT = 34
 
-def print_():
-    print("Hello World!")
+
+def make_resolution():
+    resolution = [['#'] * WIDTH for i in range(HEIGHT)]
+    for i in resolution:
+        print(''.join(list(map(str, i))))
+
+
+##def drawing():
+
+
+
+def main():
+    make_resolution()
 
 
 if __name__ == '__main__':
-    print_()
+    main()
