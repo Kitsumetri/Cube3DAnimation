7690b8f2edf123c98d18c508351824c220c4a30e
Kitsu <krasilnikovkolya4654@gmail.com>
2022-03-29 20:08:00 +0300

    created non-working resolution class

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ WIDTH = 170
 HEIGHT = 34
 
 
-def make_resolution():
-    resolution = [['#'] * WIDTH for i in range(HEIGHT)]
-    for i in resolution:
-        print(''.join(list(map(str, i))))
+class Resolution:
+
+    def make_resolution(self):
+        resolution = [['#'] * WIDTH for i in range(HEIGHT)]
+        for i in resolution:
+            print(''.join(list(map(str, i))))
 
 
-##def drawing():
-
+    def drawing(self, resolution):
+        for i in range (WIDTH):
+            for j in range(HEIGHT):
+                x = (i/WIDTH)*2
+                y = (j/HEIGHT)*2
+                if abs(x)+abs(y) == 1:
+                    resolution[i][j] = '@'
+        for i in resolution:
+            print(''.join(list(map(str, i))))
 
 
 def main():
-    make_resolution()
-
 
 if __name__ == '__main__':
     main()
