57224b35c3b9f05c07664cbe9ce6cf8d5ac44c75
Kitsu <krasilnikovkolya4654@gmail.com>
2022-03-30 20:41:43 +0300

    drawing in console

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
-
 # Resolution
 WIDTH = 170
 HEIGHT = 34
 
-
-class Resolution:
-
-    def make_resolution(self):
-        resolution = [['#'] * WIDTH for i in range(HEIGHT)]
-        for i in resolution:
-            print(''.join(list(map(str, i))))
+RTX = tuple()
 
 
-    def drawing(self, resolution):
-        for i in range (WIDTH):
-            for j in range(HEIGHT):
-                x = (i/WIDTH)*2
-                y = (j/HEIGHT)*2
-                if abs(x)+abs(y) == 1:
-                    resolution[i][j] = '@'
-        for i in resolution:
-            print(''.join(list(map(str, i))))
+def make_resolution():
+    resolution = [['!'] * WIDTH for i in range(HEIGHT)]
+    return resolution
+
+
+def print_drawing(drawing):
+    for i in drawing:
+        print(''.join(list(map(str, i))))
+
+
+def drawing():
+    resolution = make_resolution()
+    x_0 = 17
+    y_0 = 85
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+
+            if i > 85:
+                x = x_0 - i
+            else:
+                x = x_0 + i
+
+            if j > 17:
+                y = y_0 - j
+            else:
+                y = y_0 + j
+
+            if abs(x) + abs(y) <= 50:
+                resolution[i][j] = '@'
+    print_drawing(resolution)
 
 
 def main():
+    drawing()
+
 
 if __name__ == '__main__':
     main()
