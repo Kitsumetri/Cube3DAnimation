953026e7271a7835afe0a669de39cfc2e922408d
Kitsu <krasilnikovkolya4654@gmail.com>
2022-04-01 00:02:33 +0300

    added lights and shadows on 2d figure

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from math import cos, sin, sqrt, pi
 
-# Resolution
+# Resolution (WIDTH/HEIGHT = 5/1)
 WIDTH = 300
 HEIGHT = 60
 
-
-RTX = tuple(" .:!/r(lZ4HW8$@")
+RTX = tuple(" .:!/r(l1Z4H9W8$@")
 
 
 def make_resolution():
@@ -22,19 +21,19 @@ def drawing():
 
     x_0 = round(HEIGHT/2)
     y_0 = round(WIDTH/2)
-    aspect = (11*WIDTH)/(24*HEIGHT)
 
     for t in range(100000):
         screen = make_resolution()
         for i in range(-2*x_0, x_0):
             for j in range(-2*y_0, y_0):
 
-                default_x = (x_0 + i) * aspect
+                default_x = (x_0 + i) * (11*WIDTH)/(24*HEIGHT)
                 default_y = (y_0 + j) + 60
 
                 # Rotation
-                x = (default_x * cos(t*0.01) - default_y * sin(t*0.01))
-                y = (default_x * sin(t*0.01) + default_y * cos(t*0.01))
+                rotation_speed = t * 0.01
+                x = (default_x * cos(rotation_speed) - default_y * sin(rotation_speed))
+                y = (default_x * sin(rotation_speed) + default_y * cos(rotation_speed))
 
                 # X-axis movement
                 '''
@@ -48,6 +47,7 @@ def drawing():
                 y = ((default_x + sin(t * 0.003) * 20) * cos(pi / 4) + default_y * sin(pi / 4))
                 '''
 
+                # Lights and shadows
                 color = round(sqrt(x**2 + y**2)/2)
                 if color < 0:
                     color = 0
@@ -55,10 +55,12 @@ def drawing():
                     color = len(RTX)
 
                 # Square
-                if abs(x) + abs(y) <= 35:
+                if abs(x) + abs(y) <= 38:
                     screen[i][j] = RTX[-color]
+                    # Core color
                     if color == 0:
                         screen[i][j] = '0'
+
         print_drawing(screen)
 
 
