8dda0ced80f1fc5a17f063065b41b443c7442ec6
Kitsu <krasilnikovkolya4654@gmail.com>
2022-03-31 21:01:23 +0300

    started animating 2D

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from math import cos, sin, pi
+
 # Resolution
 WIDTH = 170
 HEIGHT = 34
 
-RTX = tuple()
+
+RTX = tuple(" .:!/r(l1Z4H9W8$@")
 
 
 def make_resolution():
-    resolution = [['!'] * WIDTH for i in range(HEIGHT)]
+    resolution = [[' '] * WIDTH for i in range(HEIGHT)]
     return resolution
 
 
@@ -16,25 +19,21 @@ def print_drawing(screen):
 
 
 def drawing():
+
     screen = make_resolution()
-    x_0 = 17
-    y_0 = 85
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
+    x_0 = round(HEIGHT/2)  # 17
+    y_0 = round(WIDTH/2)   # 85
+    aspect = (11*WIDTH)/(24*HEIGHT)
 
-            if i > 85:
-                x = x_0 - i
-            else:
-                x = x_0 + i
-
-            if j > 17:
-                y = y_0 - j
-            else:
-                y = y_0 + j
-
-            if abs(x) + abs(y) <= 50:
-                screen[i][j] = '@'
-    print_drawing(screen)
+    for t in range(0, 10):
+        for i in range(-2*x_0, x_0):
+            for j in range(-2*y_0, y_0):
+                default_x = (x_0 + i) * aspect + sin(t)
+                x = (default_x * cos(pi/4) - (y_0 + j) * sin(pi/4))
+                y = (default_x * sin(pi/4) + (y_0 + j) * cos(pi/4))
+                if abs(x) + abs(y) <= 20:
+                    screen[i][j] = '@'
+        print_drawing(screen)
 
 
 def main():
