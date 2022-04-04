d6457311c5891c07e0d3f3e22cb0af792b59137b
Kitsu <krasilnikovkolya4654@gmail.com>
2022-04-04 22:38:22 +0300

    code redesign

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from math import cos, sin, sqrt, pi
+import numpy as np
 
 # Resolution (WIDTH/HEIGHT = 5/1)
 WIDTH = 300
 HEIGHT = 60
 
-RTX = tuple(" .:!/r(l1Z4H9W8$@")
+RTX = tuple(" .:!/)(l1Z4H9W8$@")
+
+
+def rotate_y_axis(default_x, default_y, t):
+    x = ((default_x + sin(t * 0.003) * 20) * cos(pi / 4) - default_y * sin(pi / 4))
+    y = ((default_x + sin(t * 0.003) * 20) * cos(pi / 4) + default_y * sin(pi / 4))
+
+    return x, y
+
+
+def rotate_x_axis(default_x, default_y, t):
+    x = (default_x * cos(pi / 4) - (default_y + sin(t * 0.003) * 50) * sin(pi / 4))
+    y = (default_x * cos(pi / 4) + (default_y + sin(t * 0.003) * 50) * sin(pi / 4))
+
+    return x, y
+
+
+def rotation(default_x, default_y, rotation_speed):
+    x = (default_x * cos(rotation_speed) - default_y * sin(rotation_speed))
+    y = (default_x * sin(rotation_speed) + default_y * cos(rotation_speed))
+
+    return x, y
 
 
 def make_resolution():
@@ -15,6 +37,9 @@ def make_resolution():
 def print_drawing(screen):
     for i in screen:
         print(''.join(list(map(str, i))))
+
+
+vec = []
 
 
 def drawing():
@@ -32,20 +57,17 @@ def drawing():
 
                 # Rotation
                 rotation_speed = t * 0.01
-                x = (default_x * cos(rotation_speed) - default_y * sin(rotation_speed))
-                y = (default_x * sin(rotation_speed) + default_y * cos(rotation_speed))
+                x, y = rotation(default_x, default_y, rotation_speed)
 
                 # X-axis movement
-                '''
-                x = (default_x * cos(pi/4) - (default_y + sin(t*0.003)*50) * sin(pi/4))
-                y = (default_x * cos(pi/4) + (default_y + sin(t*0.003)*50) * sin(pi/4))
-                '''
+                # x, y = rotate_x_axis(default_x, default_y, t)
 
                 # Y-axis movement
-                '''
-                x = ((default_x + sin(t * 0.003) * 20) * cos(pi / 4) - default_y * sin(pi / 4))
-                y = ((default_x + sin(t * 0.003) * 20) * cos(pi / 4) + default_y * sin(pi / 4))
-                '''
+                # x, y = rotate_y_axis(default_x, default_y, t)
+
+                vec.append(x)
+                vec.append(y)
+                vector = np.array(vec)
 
                 # Lights and shadows
                 color = round(sqrt(x**2 + y**2)/2)
@@ -60,6 +82,8 @@ def drawing():
                     # Core color
                     if color == 0:
                         screen[i][j] = '0'
+
+                vec.clear()
 
         print_drawing(screen)
 
